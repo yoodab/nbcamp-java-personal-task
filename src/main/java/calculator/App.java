ed6ebdc0e05a09e321b9e9aeb28e86a9a07103b2
yoodab <yoondab2166@gmail.com>
2024-04-30 14:03:41 +0900

    level 1-2 Scanner를 사용하여 사칙연산 기호를 전달받기

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -8,12 +8,12 @@ public class App {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("첫 번째 숫자를 입력하세요: ");
-        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
         int num1 = sc.nextInt();
 
         System.out.print("두 번째 숫자를 입력하세요: ");
-        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
         int num2 = sc.nextInt();
 
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        char operator = sc.next().charAt(0);
     }
 }
