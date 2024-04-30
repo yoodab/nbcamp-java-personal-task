4c6636fcee6ec3fb2de90c6b727c4aa5201ddd89
yoodab <yoondab2166@gmail.com>
2024-04-30 14:11:57 +0900

    level 1-3 정수 2개와 사칙연산 기호를 입력받아 연산 진행 후 결과값 출력

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -15,5 +15,27 @@ public class App {
 
         System.out.print("사칙연산 기호를 입력하세요: ");
         char operator = sc.next().charAt(0);
+
+        int result = 0;
+        switch (operator){
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                // 두번째 정수가 0이 들어가는 경우
+                if(num2==0){
+                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                }else {
+                    result = num1 / num2;
+                }
+                break;
+        }
+        System.out.println("결과: " + result);
     }
 }
